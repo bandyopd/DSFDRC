6ded3def3b1a2065f13b42798bbe8fde6ad11d1f
ATIKA FARZANA URMI <urmiaf@mymail.vcu.edu>
2023-01-05 13:24:19 -0500

    2nd version

--- R/copy_d_n2.R
+++ R/copy_d_n2.R
@@ -1,10 +1,11 @@
-#' creating knockoff copies of covairates screened in 1st step.Here we put the bigger data (n2)
-#' with only the covariates selected in the screening step
+# creating knockoff copies of covairates screened in 1st step.Here we put the bigger data (n2) with
+# only the covariates selected in the screening step
 ##' @title copy_d_n2
 ##' @return a data frame of time,delta and knockoff copies of original covariates
 ##' @author Atika Farzana Urmi
 ##' @param data_d_n2 A data frame of time,delta and original 'd' covariates
 ##' @param rand_num a random number which was used in the data splitting step
+##' @noRd
 copy_d_n2 <- function(data_d_n2, rand_num) {
     xo <- as.matrix(data_d_n2[, -c(1, 2)])  #original x
     # normalizing constant
